ef4c8665d895c724d12ce2d6c27813360357acc7
Unknown <janpo14@uia.no>
2017-10-31 13:53:26 +0100

    endret mappestruktur  og separate send/motta programm

---
+++ del3/Oppgave3_send/Oppgave3_send.ino
@@ -0,0 +1,128 @@
+#include "MPU6050.h"
+#include "FlexCAN.h"
+#include "Metro.h"
+
+#ifndef __MK66FX1M0__
+#error "Teensy 3.6 with dual CAN bus is required to run this example"
+#endif
+
+MPU6050 mpu1;                   //Oppretter MPU6050 "mpu1"
+
+
+CAN_message_t led_toggle;       //Melding for å toggle led ved oppstart
+
+CAN_message_t led_OnOff;        //Meldign for å styre led av/på
+
+CAN_message_t MPU6050;          //Melding for å lese av verdiene på MPU6050
+
+
+Metro serialMetro = Metro(1000);
+
+
+struct ACCL                     //Oppretter struct for serialisering av MPU6050 signal
+{
+  float acclZ;
+};
+
+
+ACCL mpu6050;                   //variabel mpu6050 av type ACCL
+
+const int toggleId = 0x22;
+const int onOffId = 0x21;
+const int MPU6050Id = 0x20;
+
+void setup() {
+  // put your setup code here, to run once:
+  delay(2000);
+  Serial.println(F("Hello Teensy 3.6 dual CAN Test."));
+
+  mpu1.init();
+
+
+
+  struct CAN_filter_t defaultMask;     // defaultMask filter med egenskaper tilsvarende default i FlexCAN.h
+  defaultMask.flags.remote = 0;
+  defaultMask.flags.extended = 0;
+  defaultMask.id = 0;
+
+  Can0.begin(250000, defaultMask, true, true);  //alternativ pin-tilkobling av Can0
+
+  Can1.begin(250000, defaultMask, false, false);
+
+
+  //if using enable pins on a transceiver they need to be set on
+  pinMode(28, OUTPUT);
+  pinMode(35, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  digitalWrite(28, LOW);    //NB!!!! LOW == High speed mode
+  digitalWrite(35, LOW);
+  digitalWrite(13, LOW);
+
+  MPU6050.ext = 0;
+  MPU6050.id = MPU6050Id;
+  MPU6050.len = sizeof(mpu6050);
+
+  led_OnOff.ext = 0;
+  led_OnOff.id = onOffId;
+  led_OnOff.len = 1;
+
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ if (serialMetro.check() == 1) 
+ {
+  Can1.write(MPU6050);
+  CAN_message_t inMsg;
+
+  Can0.read(inMsg);
+
+  switch (inMsg.id) {
+    case 0x20:
+
+      Serial.println("0x20");
+
+      break;
+
+    case 0x21:
+
+      Serial.println("0x21");
+      
+      break;
+
+    default:
+
+      break;
+  }
+
+}
+
+
+
+
+
+
+}
+
+
+void imudata()
+{
+  Serial.print("faktisk verdi mpu1.acclZ(): ");
+  Serial.println(mpu1.acclZ());
+
+  mpu6050.acclZ = mpu1.acclZ();
+  memcpy(MPU6050.buf, &mpu6050, sizeof(mpu6050));   //Serialiserer MPU6050
+
+  ACCL tmp;
+
+  memcpy(&tmp, MPU6050.buf, sizeof(tmp));           //Deserialiserer MPU6050
+
+  Serial.print("Overfort verdi av acclZ: ");
+  Serial.println(tmp.acclZ);
+
+  Serial.println("*********************************");
+}
+
